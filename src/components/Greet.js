d3db3199f21e3fda7ac99bcae0347b5c1e7f58be
Akash <akashdas6881@gmail.com>
2021-10-17 01:19:56 +0530

    1st react

---
+++ src/components/Greet.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+// function Greet(){
+//     return <h1>hello das</h1>
+// }
+
+
+export const Greet = (props) => {
+
+    return(
+        <h1>hello {props.name}</h1>
+    )
+}
+
+
+//export default Greet
